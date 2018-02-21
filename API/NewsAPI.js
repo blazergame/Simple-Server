55f613e158d5bb241e1536bbc8cbc0ea187635eb
Benson Li <li_benson12@hotmail.com>
2018-02-20 22:23:30 -0800

    using firebase

--- API/NewsAPI.js
+++ API/NewsAPI.js
@@ -1,17 +1,65 @@
 'use strict';
 
+var summerize = require('summarizer').getPage;
 var axios = require('axios');
-const url = ''
+const config = require('../Config/config.json');
 
-function NewsAPI(cb) {
+//DB
+var firebase = require('firebase');
+
+//API Related 
+const NewsAPI = config.api.NewsAPI;
+const country = 'us';
+const category = 'technology';
+const apiKey = config.api.NewsAPIKey;
+
+const url = NewsAPI + 'country=' + country + '&' + 'category=' + category + '&' + 'apiKey=' + apiKey;
+
+//Store api into db, query and summarize
+firebase.initializeApp({
+  
+});
+
+
+function getNewsApi(req, res) {
 
     axios.get(url)
         .then(response => {
-            return 'aaa';
-        }).catch(error => {
+
+          // console.log(response.data);
+           //Push summerized data into global array
+           for(var i = 0; i < response.data.totalResults; i++){
+               summarizeLink(response.data.articles[i].url, (data) => {
+                    //apiObjList.append(data);
+                    console.log(data);
+                    res.send(data);
+               });
+           }
+            
+        })
+        .catch(error => {
             console.log(error.response);
         });
 
 }
 
-module.exports = NewsAPI;
+
+function summarizeLink(url, cb){
+
+    summerize(url)
+        .then(data => {
+
+            var apiObj = {
+                title : data.title,
+                // description : data.description,
+                // url : data.canonicalLink,
+                // topics : data.stats.topics,
+                // readTimeMinutes : data.stats.minutes,
+                // image : data.image,
+                // summary : data.summary
+            }
+            cb(apiObj);
+        })
+}
+
+module.exports = getNewsApi;
--- app.js
+++ app.js
@@ -3,75 +3,19 @@ const app = express();
 const config = require('./Config/config.json');
 const port = process.env.PORT || config.server.port;
 
-var axios = require('axios');
-const NewsAPI = config.api.NewsAPI;
+//List of APIs
+var NewsAPI = require('./API/NewsAPI.js');
 
-const country = 'us';
-const category = 'technology';
-const apiKey = config.api.NewsAPIKey;
-
-const url = NewsAPI + 'country=' + country + '&' + 'category=' + category + '&' + 'apiKey=' + apiKey;
-
-var summerize = require('summarizer').getPage;
-
-var apiObjList = [];
 
 app.get('/', (req, res) => {
     res.send("Hello World!");
 });
 
-app.get('/v1/news', (req, res) => {
+
+app.get('/v1/news', NewsAPI);
 
 
-    // summerize("https://www.gamespot.com/articles/fortnite-vs-pubg-which-battle-royale-game-is-right/1100-6456424/")
-    //     .then(data => {
-    //         //console.log(data);
-    //     });
 
-    //Need to summerize data to be short and consise to return back
-    axios.get(url)
-        .then(response => {
-
-           //Push summerized data into global array
-           for(var i = 0; i < response.data.totalResults; i++){
-               summarizeLink(response.data.articles[i].url, (data) => {
-                    apiObjList.push(data);
-                    console.log(data);
-               });
-
-               //TODO: Fix async issue with sending final array 
-                if(i == 20){
-                    console.log(i);
-                    res.send(apiObjList);
-                }
-               
-           }
-        })
-        .catch(error => {
-            console.log(error.response);
-        });
-
-    
-    
-});
-
-function summarizeLink(url, cb){
-
-    summerize(url)
-        .then(data => {
-
-            var apiObj = {
-                title : data.title,
-                // description : data.description,
-                // url : data.canonicalLink,
-                // topics : data.stats.topics,
-                // readTimeMinutes : data.stats.minutes,
-                // image : data.image,
-                // summary : data.summary
-            }
-            cb(apiObj);
-        })
-}
 
 app.listen(port, () => {
     console.log("Current listening on port: " + port);
